b61a30316ecd65c1c55c0c557947fd660887cede
ruzhan <bzruzhan@163.com>
2018-01-19 09:24:46 +0800

    commit 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
@@ -6,11 +7,34 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            { test: /\.scss$/,   use: ExtractTextPlugin.extract({
+
+                fallback: 'style-loader',
+
+                use: ['css-loader', 'sass-loader'],
+
+                publicPath: '/dist'
+
+            }) }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin({
         title: 'Custom template',
         minify: {
             collapseWhitespace: true
         },
         template: './src/index.html'
-    })]
+    }),
+    new ExtractTextPlugin({
+
+        filename: "app.css",
+
+        disable: false,
+
+        allChunks: true
+
+    })
+]
 }
